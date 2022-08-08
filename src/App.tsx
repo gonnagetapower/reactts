2d7488d865b8a87cad764281cfecac43f6757730
Alexandr Karpov <gonnagetapower@gmail.com>
2022-08-08 16:18:16 +0300

    adding__axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import Card, {CardVariant} from './components/Card';
+import React, { useEffect, useState } from 'react';
+import Card, { CardVariant } from './components/Card';
 import UserList from './components/UserList';
 import { IUser } from './types/types';
+import axios from 'axios';
 
 function App() {
-  const users: IUser[] = [
-    {id: 1, name: 'Gonnagetapower', email: 'gonnagetapower@gmail.com', address: {city: 'Moskva', street: 'Lenina', zipcode: '21312'}},
-    {id: 2, name: 'Karpusha', email: 'karpusha201@mail.ru', address: {city: 'Moskva', street: 'Lenina', zipcode: '55552'}}
-  ]
+  // const users: IUser[] = [
+  //   {id: 1, name: 'Gonnagetapower', email: 'gonnagetapower@gmail.com', address: {city: 'Moskva', street: 'Lenina', zipcode: '21312'}},
+  //   {id: 2, name: 'Karpusha', email: 'karpusha201@mail.ru', address: {city: 'Moskva', street: 'Lenina', zipcode: '55552'}}
+  // ]
+  const [users, setUsers] = useState<IUser[]>([])
+
+  useEffect(() => {
+    fetchUser()
+  }, [])
+
+  let fetchUser = async () => {
+    try {
+      const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+      setUsers(response.data)
+    } catch (e) {
+      alert(e)
+    }
+  }
+
+
   return (
     <div className="App">
       <Card variant={CardVariant.outlined} width='200px' height='400px' >
         <button>btn</button>
-        </Card>
-        <UserList users={users}/>
+      </Card>
+      <UserList users={users} />
     </div>
   );
 }
